e33e100c55398af26c5bcfb859f4dfb14a5c713b
silencedsre <jamakatel.shree.krishna@gmail.com>
2021-04-05 18:11:35 +0545

    add keras and tqdm

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="fuse_face_dataset",
     version="0.1.0",
     description="desc here",   
-    install_requires=['tensorflow==2.4.1','opencv-python==4.5.1.48','numpy==1.19.5','scikit-learn==0.24.1'],
+    install_requires=['tensorflow==2.4.1','opencv-python==4.5.1.48','numpy==1.19.5','scikit-learn==0.24.1', 'keras', 'tqdm'],
     zip_safe=False,
 )
 
