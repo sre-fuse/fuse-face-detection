7c5b0be38aed15fd18940f1b7c2007402f768463
silencedsre <jamakatel.shree.krishna@gmail.com>
2021-04-05 18:53:45 +0545

    update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import  setup
 
 setup(
-    name="fuse_face_dataset",
+    name="fuse_face_detection",
     version="0.1.0",
     description="desc here",   
     install_requires=['tensorflow==2.4.1','opencv-python==4.5.1.48','numpy==1.19.5','scikit-learn==0.24.1', 'keras', 'tqdm'],
